56998301fe932ba858c27a9e73d19ae925fc3469
sunil <skray1983@gmail.com>
2023-03-31 22:59:19 +0530

    product variant

---
+++ app/Models/ProductVeriant.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductVeriant extends Model
+{
+    use HasFactory;
+    protected $table = 'product_veriant';
+    protected $primaryKey = 'id';
+    /**
+     * Get the product that ows the ProductVeriant
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function products(): HasMany
+    {
+        return $this->hasMany(products::class, 'id', 'pid');
+    }
+
+}
