29f8e34aeeee2be44e8b81a0fea7c570e1329d02
drupalsunil <skray1983@gmail.com>
2023-05-24 21:33:20 +0530

    Revert "strip size to pack size"
    This reverts commit e5cb48a8beab70bef50a6e3900d038f838c336c1.

--- resources/views/admin/update_product.blade.php
+++ resources/views/admin/update_product.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin.app')
 @push('title')
-<title>Medshop | Update-Product</title>
-@endpush
+<title>Medshop | Update-Product</title>   
+@endpush  
 @section('content')
 <div class="card m-3 p-3 " style="box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;">
 <h6 class="p-2"><span style="border-bottom: 1px solid #4e73df">Update Product</span></h6>
@@ -9,7 +9,7 @@
 <form action="{{route('admin.update_product',['id'=>$product->id])}}" method="post">
 
     @csrf
-
+    
     <div class="row">
         <div class="col-md-6 mb-2">
         <label class="form-label">Title</label>
@@ -19,29 +19,29 @@
         <label class="form-label">Batch No.</label>
         <input class="form-control" type="text" name="bath_no" placeholder="Enter Product Batch No." value="{{$product->SKU}}">
         </div>
-
+    
         <div class="col-md-6 mb-2">
         <label class="form-label">MRP</label>
         <input class="form-control" type="number" min="0" step=".01"  name="mrp" placeholder="0.00" value="{{$product->MRP}}">
         </div>
-
+    
         <div class="col-md-6 mb-2">
         <label class="form-label">Price/unit</label>
         <input class="form-control" type="number" min="0" step=".01" name="price" placeholder="0.00" value="{{$product->Price_unit}}">
         </div>
-
-
-
+    
+        
+    
         <div class="col-md-6 mb-2">
         <label class="form-label">Stock</label>
         <input class="form-control" type="number" min="0" name="stock" placeholder="Enter Product Stock" value="{{$product->Stock}}">
         </div>
-
+    
         <div class="col-md-6 mb-2">
         <label class="form-label">Exp. date</label>
         <input class="form-control" type="date"  name="exp_date"  placeholder="Enter Product Exp date" value="{{$product->Exp_date}}">
         </div>
-
+    
         <div class="col-md-6 mb-2">
         <label class="form-label">Category</label>
         <select class="form-control"  name="category" aria-label="Default select example">
@@ -50,11 +50,11 @@
             <option value="{{$item->Categories_id}}" @selected($product->Categories_id == $item->Categories_id)
                 @class([
                 'bg-purple-600 text-white' => $product->Categories_id == $item->Categories_id
-                ])>{{$item->Name}}</option>
+                ])>{{$item->Name}}</option>    
             @endforeach
           </select>
         </div>
-
+    
         <div class="col-md-6 mb-2">
             <label class="form-label">Company(brand)</label>
             <select class="form-control" name="brand" aria-label="Default select example">
@@ -63,16 +63,16 @@
                 <option value="{{$item->id}}" @selected($product->Brand == $item->id)
                     @class([
                     'bg-purple-600 text-white' => $product->Brand == $item->id
-                    ])>{{$item->Name}}</option>
+                    ])>{{$item->Name}}</option>    
                 @endforeach
             </select>
-        </div>
-
+        </div> 
+    
         <div class="col-md-6 mb-2">
             <label class="form-label">Box No.</label>
-            <input type="text" class="form-control" name="box_no" value="{{$product->Box_No}}" placeholder="Enter Product Box No.">
+            <input type="text" class="form-control" name="box_no" value="{{$product->Box_No}}" placeholder="Enter Product Box No."> 
         </div>
-
+    
         <div class="col-md-6 mb-2">
             <label class="form-label">Function</label>
             <select class="form-control" name="function" aria-label="Default select example">
@@ -81,21 +81,21 @@
                 <option value="{{$item->id}}" @selected($product->Function == $item->id)
                     @class([
                     'bg-purple-600 text-white' => $product->Function == $item->id
-                    ])>{{$item->Name}}</option>
+                    ])>{{$item->Name}}</option>    
                 @endforeach
             </select>
-        </div>
-
+        </div> 
+    
         <div class="col-md-6 mb-2">
             <label class="form-label">Generic name</label>
-            <input type="text" class="form-control" name="generic_name" value="{{$product->Generic_name}}" placeholder="Enter Product Generic name">
-        </div>
-
+            <input type="text" class="form-control" name="generic_name" value="{{$product->Generic_name}}" placeholder="Enter Product Generic name"> 
+        </div> 
+    
         <div class="col-md-6 mb-2">
             <label class="form-label">Ingredients</label>
             <input type="text" class="form-control" name="ingredients" value="{{$product->Ingredients}}" placeholder="Enter Product Ingredients">
-        </div>
-
+        </div> 
+    
         <div class="col-md-6 mb-2">
             <label class="form-label">Schedule</label>
             <select class="form-control" name="schedule" >
@@ -104,21 +104,21 @@
                 <option value="{{$item->id}}" @selected($product->Schedule == $item->id)
                     @class([
                     'bg-purple-600 text-white' => $product->Schedule == $item->id
-                    ])>{{$item->Name}}</option>
+                    ])>{{$item->Name}}</option>    
                 @endforeach
             </select>
             <span class="text-danger text-capitalize">
             @error('schedule')
-            {{$message}}
+            {{$message}}       
             @enderror
             </span>
-        </div>
+        </div> 
 
         <div class="col-md-6 mb-2">
-            <label class="form-label">Pack Size</label>
+            <label class="form-label">Trip Size</label>
             <input type="text" class="form-control" name="tripsize"  value="{{$product->TripSize}}"  placeholder="Enter Product Trip-Size">
-        </div>
-
+        </div> 
+    
         <div class="col-md-12">
           <label for="exampleFormControlTextarea1" class="form-label">Description</label>
           <textarea class="form-control" name="description"  rows="3">{{$product->Description}}</textarea>
