49a1ff9ba4f3ad1aed48421e2f73bb3f42cd8d2b
Nikhil <nikhil.gupta@rakolsoft.com>
2023-06-06 16:56:24 +0530

    issue 3

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -9,51 +9,59 @@ use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    public function index(){
-     
-    return view('admin.add_category');
+    public function index()
+    {
+
+        return view('admin.add_category');
     }
 
-    public function view(){
-    $category = Category::paginate(25);  
-    return view('admin.view_category')->with(compact('category'));
+    public function view()
+    {
+        $category = Category::paginate(25);
+        return view('admin.view_category')->with(compact('category'));
     }
 
-    public function store(Request $req){
+    public function store(Request $req)
+    {
         $req->validate(
             [
-                'name'=>'required|unique:categories',
-            ]);
-    $category = new Category;
-    $category->Name = $req['name'];
-    $category->HSN = $req['HSN']; 
-    $category->Gstrate = $req['gst_rate']; 
-    $category->save();
-    return redirect (route('admin.view_category'))->with(Session::flash('message', "Category Added!"));
-     
+                'name' => 'required|unique:categories',
+            ]
+        );
+        $category = new Category;
+        $category->Name = $req['name'];
+        $category->HSN = $req['HSN'];
+        $category->Gstrate = $req['gst_rate'];
+        $category->description = $req['description'];
+        $category->save();
+        return redirect(route('admin.view_category'))->with(Session::flash('message', "Category Added!"));
     }
 
-    public function delete($id){
-    $category = Category::find( $id);
-    if ( $category != null) {
-        $category->delete();
-    }
-   return redirect()->back()->with('msg-delete','Category Deleted!');
-    }
-
-    public function edit($id){
-    $category = Category::find($id);
-    if ($category != null) {
-        return view('admin.update_category')->with(compact('category'));
-    }
+    public function delete($id)
+    {
+        $category = Category::find($id);
+        if ($category != null) {
+            $category->delete();
+        }
+        return redirect()->back()->with('msg-delete', 'Category Deleted!');
     }
 
-    public function update(Request $req ,$id){
-    $category= Category::find($id);
-    $category->Name = $req['name'];
-    $category->HSN = $req['HSN'];
-    $category->Gstrate = $req['gstrate'];
-    $category->save();
-    return redirect (route('admin.view_category'))->with(Session::flash('message-updated', "Category updated!"));
+    public function edit($id)
+    {
+        $category = Category::find($id);
+        if ($category != null) {
+            return view('admin.update_category')->with(compact('category'));
+        }
+    }
+
+    public function update(Request $req, $id)
+    {
+        $category = Category::find($id);
+        $category->Name = $req['name'];
+        $category->HSN = $req['HSN'];
+        $category->Gstrate = $req['gstrate'];
+        $category->description = $req['description'];
+        $category->save();
+        return redirect(route('admin.view_category'))->with(Session::flash('message-updated', "Category updated!"));
     }
 }
